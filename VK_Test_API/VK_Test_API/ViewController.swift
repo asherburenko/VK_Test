1f3572fc6de199e06062721d42a1084c44b089f4
A.Sherburenko <asherburenko@mail.ru>
2022-03-30 20:27:24 +0500

    create new project VK_Test_API

---
+++ VK_Test_API/VK_Test_API/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VK_Test_API
+//
+//  Created by Александр Шербуренко on 30.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
