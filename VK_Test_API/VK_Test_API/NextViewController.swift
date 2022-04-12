28cd94f40773ef7c16306192a05e8113984944f3
A.Sherburenko <asherburenko@mail.ru>
2022-04-12 22:38:37 +0500

    Преобразовал json-ответ по всем запросам

--- VK_Test_API/VK_Test_API/NextViewController.swift
+++ VK_Test_API/VK_Test_API/NextViewController.swift
@@ -7,11 +7,16 @@
 
 import UIKit
 import WebKit
+import Alamofire
+import SwiftyJSON
 
 class NextViewController: UIViewController {
+    
+    let host = "https://api.vk.com"
 
     override func viewDidLoad() {
         super.viewDidLoad()
+//        loadUsers()
         getPhotos()
         getGroupsList()
         getFriendsList()
@@ -21,100 +26,144 @@ class NextViewController: UIViewController {
 
 extension NextViewController {
     func getFriendsList() {
-        let configuration = URLSessionConfiguration.default
-        let session = URLSession(configuration: configuration)
-        var urlComponents = URLComponents()
-        urlComponents.scheme = "https"
-        urlComponents.host = "api.vk.com"
-        urlComponents.path = "/method/friends.get"
-        urlComponents.queryItems = [
-            URLQueryItem(name: "user_id", value: String(Session.shared.userID)),
-            URLQueryItem(name: "order", value: "random"),
-            //URLQueryItem(name: "count", value: "10"),
-            URLQueryItem(name: "access_token", value: Session.shared.token),
-            URLQueryItem(name: "v", value: "5.131")
+        
+        let path = "/method/friends.get"
+        
+        let parameters = [
+            "user_id": String(Session.shared.userID),
+            "order": "random",
+            "access_token": Session.shared.token,
+            "v": "5.131"
         ]
-        var request = URLRequest(url: urlComponents.url!)
-        request.httpMethod = "GET"
-        let task = session.dataTask(with: request) { (data, response, error) in
-            let json = try? JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.allowFragments)
-            print("FriendList", json)
-        }
-        task.resume()
+        
+        AF
+            .request(host + path,
+                     method: .get,
+                     parameters: parameters)
+            .responseData { response in
+                switch response.result {
+                case .success(let data):
+                    let json = JSON(data)
+                    let friends = Friends0(json)
+                    print(friends)
+                case .failure(let error):
+                    print(error)
+                }
+            }
     }
 }
 
 extension NextViewController {
     func getPhotos() {
-        let configuration = URLSessionConfiguration.default
-        let session = URLSession(configuration: configuration)
-        var urlComponents = URLComponents()
-        urlComponents.scheme = "https"
-        urlComponents.host = "api.vk.com"
-        urlComponents.path = "/method/photos.get"
-        urlComponents.queryItems = [
-            URLQueryItem(name: "owner_id", value: String(Session.shared.userID)),
-            URLQueryItem(name: "album_id", value: "profile"),
-            URLQueryItem(name: "rev", value: "0"),
-            URLQueryItem(name: "access_token", value: Session.shared.token),
-            URLQueryItem(name: "v", value: "5.131")
+        let path = "/method/photos.get"
+        
+        let parameters = [
+            "owner_id": String(Session.shared.userID),
+            "album_id": "profile",
+            "rev": "0",
+            "access_token": Session.shared.token,
+            "v": "5.131"
         ]
-        var request = URLRequest(url: urlComponents.url!)
-        request.httpMethod = "GET"
-        let task = session.dataTask(with: request) { (data, response, error) in
-            let json = try? JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.allowFragments)
-            print("Photo", json)
-        }
-        task.resume()
+        
+        AF
+            .request(host + path,
+                     method: .get,
+                     parameters: parameters)
+            .responseData { response in
+                switch response.result {
+                case .success(let data):
+                    print(data)
+                    let json = JSON(data)
+                    let photos = Photos0(json)
+                    print(photos)
+                case .failure(let error):
+                    print(error)
+                }
+            }
     }
 }
 
 extension NextViewController {
     func getGroupsList() {
-        let configuration = URLSessionConfiguration.default
-        let session = URLSession(configuration: configuration)
-        var urlComponents = URLComponents()
-        urlComponents.scheme = "https"
-        urlComponents.host = "api.vk.com"
-        urlComponents.path = "/method/groups.get"
-        urlComponents.queryItems = [
-            URLQueryItem(name: "user_id", value: String(Session.shared.userID)),
-            URLQueryItem(name: "extended", value: "0"),
-            URLQueryItem(name: "access_token", value: Session.shared.token),
-            URLQueryItem(name: "v", value: "5.131")
+        let path = "/method/groups.get"
+        
+        let parameters = [
+            "user_id": String(Session.shared.userID),
+            "extended": "0",
+            "access_token": Session.shared.token,
+            "v": "5.131"
         ]
-        var request = URLRequest(url: urlComponents.url!)
-        request.httpMethod = "GET"
-        let task = session.dataTask(with: request) { (data, response, error) in
-            let json = try? JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.allowFragments)
-            print("Groups", json)
-        }
-        task.resume()
+        
+        AF
+            .request(host + path,
+                     method: .get,
+                     parameters: parameters)
+            .responseData { response in
+                switch response.result {
+                case .success(let data):
+                    let json = JSON(data)
+                    let groups = Groups(json)
+                    print(groups)
+                case .failure(let error):
+                    print(error)
+                }
+            }
     }
 }
 
 extension NextViewController {
     func getSearchGroupsList() {
-        let configuration = URLSessionConfiguration.default
-        let session = URLSession(configuration: configuration)
-        var urlComponents = URLComponents()
-        urlComponents.scheme = "https"
-        urlComponents.host = "api.vk.com"
-        urlComponents.path = "/method/groups.search"
-        urlComponents.queryItems = [
-            URLQueryItem(name: "q", value: "Music"),
-           // URLQueryItem(name: "type", value: "group"),
-            URLQueryItem(name: "offset", value: "3"),
-            URLQueryItem(name: "count", value: "3"),
-            URLQueryItem(name: "access_token", value: Session.shared.token),
-            URLQueryItem(name: "v", value: "5.131")
+        let path = "/method/groups.search"
+        
+        let parameters = [
+            "q": "Music",
+            "offset": "3",
+            "count": "3",
+            "access_token": Session.shared.token,
+            "v": "5.131"
         ]
-        var request = URLRequest(url: urlComponents.url!)
-        request.httpMethod = "GET"
-        let task = session.dataTask(with: request) { (data, response, error) in
-            let json = try? JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.allowFragments)
-            print("Search", json)
-        }
-        task.resume()
+        
+        AF
+            .request(host + path,
+                     method: .get,
+                     parameters: parameters)
+            .responseData { response in
+                switch response.result {
+                case .success(let data):
+                    print(data)
+                    let json = JSON(data)
+                    let search = Search(json)
+                    print(search)
+                case .failure(let error):
+                    print(error)
+                }
+            }
+    }
+}
+
+extension NextViewController {
+    func loadUsers() {
+        AF
+            .request("https://jsonplaceholder.typicode.com/users", method: .get)
+            .responseData { response in
+                switch response.result {
+                case .success(let data):
+                    print(data)
+                    let json = JSON(data)
+                    let users = json.arrayValue.compactMap{ User0($0) }
+                    print(users)
+//                    print(data)
+//                    let json = try? JSONSerialization.jsonObject(with: data, options: .allowFragments)
+//
+//                    let decoder = JSONDecoder()
+//                    let user = try? decoder.decode([User].self, from: data)
+//                    user?.forEach{ user in
+//                        print("\(user.id): \(user.name)")
+//                    }
+//                    print(json)
+                case .failure(let error):
+                    print(error)
+                }
+            }
     }
 }
